89634058920697a25fde65e2e7ee7c9e202645a3
B. Morgan Murrah <beau@airbridge.ac.nz>
2016-08-16 18:04:11 -0400

    Starter Code 'test it all out' reached

---
+++ espm/models/user.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+var Todo = require('./todo');
+
+var UserSchema = new mongoose.Schema({
+  local : {
+    email    : String,
+    password : String
+  },
+  todos : [Todo.schema]
+});
+
+UserSchema.methods.encrypt = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+};
+
+UserSchema.methods.isValidPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
+
+// Starter code
+// Todos linked to embedeed document under Todo. Maybe needs change
