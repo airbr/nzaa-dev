10172bd93833aae594ffea5a6958661fa4c5e9ab
B. Morgan Murrah <beau@airbridge.ac.nz>
2016-08-24 10:04:09 -0400

    Minor style changes

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ if (process.env.MONGODB_URI) {
   mongoose.connect(process.env.MONGODB_URI);
 }
 else {
-  mongoose.connect('mongodb://localhost/express-movies');
+  mongoose.connect('mongodb://localhost/espm');
 }
 mongoose.connection.on('error', function(err) {
   console.error('MongoDB connection error: ' + err);
