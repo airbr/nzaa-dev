2f8b878847999d063735b9c1a4b087d2335d86c6
B. Morgan Murrah <beau@airbridge.ac.nz>
2016-08-24 02:02:40 -0400

    adding mongolab support

--- app.js
+++ app.js
@@ -27,9 +27,27 @@ var app = express();
 app.use('/static', express.static('public/images/'));
 
 
+// // Connect to database
+// mongoose.connect('mongodb://localhost/espm');
+// // mongoose.connect('mongodb://localhost/minutes');
+
 // Connect to database
-mongoose.connect('mongodb://localhost/espm');
-// mongoose.connect('mongodb://localhost/minutes');
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/express-movies');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
+
+
 
 
 // view engine setup
