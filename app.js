9c3caed873c88944aa80927faa476ffb3dd3c9f4
Morgan Murrah <mmurrah@airbridge.ac.nz>
2017-05-01 19:35:46 -0400

    Tidying App.js

--- app.js
+++ app.js
@@ -1,31 +1,25 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-// Below added VV
+
 var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
-// Above added ^^
+
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var methodOverride = require('method-override');
 
-
 // Routes
 var homeRouter = require('./routes/index');
 var minutesRouter = require('./routes/minutes');
-
 var usersRouter = require('./routes/users');
-var todosRouter = require('./routes/todos');
-
-
 var app = express();
 
 // Use Static Content
 app.use('/static', express.static('public/images/'));
-
 // Connect to database
 if (process.env.MONGODB_URI) {
   mongoose.connect(process.env.MONGODB_URI);
@@ -42,13 +36,11 @@ mongoose.connection.once('open', function() {
   console.log("Mongoose has connected to MongoDB!");
 });
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -56,7 +48,6 @@ app.use(cookieParser());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Cust
 app.use(session({ secret: 'WDI Rocks!',
                   resave: true,
                   saveUninitialized: true }));
@@ -76,8 +67,6 @@ app.use(function (req, res, next) {
 app.use('/', homeRouter);
 app.use('/minutes', minutesRouter);
 app.use('/users', usersRouter);
-app.use('/todos', todosRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -85,8 +74,6 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
-// error handlers
 
 // development error handler
 // will print stacktrace
@@ -100,8 +87,7 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
+// production error handler - no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
@@ -111,5 +97,4 @@ app.use(function(err, req, res, next) {
 });
 
 console.log('Running in %s mode', app.get('env'));
-
 module.exports = app;
