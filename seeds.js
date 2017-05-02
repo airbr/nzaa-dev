cfb4676acd5485c0f5c8f86634de967b2ff1b19a
Morgan Murrah <mmurrah@airbridge.ac.nz>
2017-05-01 20:06:25 -0400

    barebones EJS template conditional for if no minutes exist yet.

--- seeds.js
+++ seeds.js
@@ -1,16 +1,15 @@
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-var User = require('./models/user');
+// var User = require('./models/user');
 var Minutes = require('./models/minutes');
 
 mongoose.connect('mongodb://localhost/nzaa');
 
-// our script will not exit until we have disconnected from the db.
+// Script will not exit until disconnected from the db.
 function quit() {
   mongoose.disconnect();
   console.log('\nQuitting!');
 }
-
 // a simple error handler
 function handleError(err) {
   console.log('ERROR:', err);
@@ -18,133 +17,5 @@ function handleError(err) {
   return err;
 }
 
-console.log('removing old minutes...');
-Minutes.remove({})
-// .then(function() {
-//   var admin = new User( {} );
-//   admin.local = {
-//     email : 'admin@admin.com',
-//     password : admin.encrypt('admin')
-//   };
-//   admin.role = 'boardMember';
-//   return User.create(admin);
-// });
 quit();
 
-
-
-
-
-
-
-
-// .then(function() {
-//   console.log('old minutes removed');
-//   console.log('removing old users...');
-//   return User.remove({})
-// })
-// .then(function() {
-//   var joe = new User( {} );
-//   joe.local = {
-//     email : 'joe@hacker.com',
-//     password : joe.encrypt('test1234')
-//   };
-//   joe.role = 'boardMember';
-//   return User.create(joe);
-// })
-// .then(function(joe) {
-//   console.log('Created user:', joe);
-//   console.log('creating some new minutes...');
-  // ß
-  // dateOf:         { type: Date,  required: true },
-  // type:           { type: String, required: true },
-  // approved:     { type: Boolean, required: true },
-  // headline:     { type: String, required: true}
-
-
-//   var minutes     = new Minutes({ dateOf: Date.now(),
-//                                   type: 'Regular Meeting',
-//                                   approved: true,
-//                                   headline: 'Best Meeting Ever',
-//                                   author: ' Author '
-//                                 });
-//   return Minutes.create(minutes);
-// })
-// .then(function(savedMinutes) {
-//   console.log('Just saved', savedMinutes);
-//   return Minutes.find({});
-// })
-// .then(function(allMinutes) {
-//   console.log('Printing all minutes:');
-//   allMinutes.forEach(function(minutes) {
-//     console.log(minutes);
-//   });
-//   quit();
-// });
-
-
-// currentUser.objectID
- // var Meeting = {
- //    dateOf: 2016-08-22,
- //    type: 'Board Meeting',
- //    approved: false,
- //    headline: 'This is the headline of the minutes'
- //  };
-
-
-// var mongoose = require('mongoose');
-// var Todo = require('./models/todo');
-
-// mongoose.connect('mongodb://localhost/todos');
-
-// // our script will not exit until we have disconnected from the db.
-// function quit() {
-//   mongoose.disconnect();
-//   console.log('\nQuitting!');
-// }
-
-// // a simple error handler
-// function handleError(err) {
-//   console.log('ERROR:', err);
-//   quit();
-//   return err;
-// }
-
-// console.log('removing old todos...');
-// Todo.remove({})
-// .then(function() {
-//   console.log('old todos removed');
-//   console.log('creating some new todos...');
-//   var groceries  = new Todo({ title: 'groceries',    completed: false });
-//   var feedTheCat = new Todo({ title: 'feed the cat', completed: true  });
-//   return Todo.create([groceries, feedTheCat]);
-// })
-// .then(function(savedTodos) {
-//   console.log('Just saved', savedTodos.length, 'todos.');
-//   return Todo.find({});
-// })
-// .then(function(allTodos) {
-//   console.log('Printing all todos:');
-//   allTodos.forEach(function(todo) {
-//     console.log(todo);
-//   });
-//   return Todo.findOne({title: 'groceries'});
-// })
-// .then(function(groceries) {
-//   groceries.completed = true;
-//   return groceries.save();
-// })
-// .then(function(groceries) {
-//   console.log('updated groceries:', groceries);
-//   return groceries.remove();
-// })
-// .then(function(deleted) {
-//   return Todo.find({});
-// })
-// .then(function(allTodos) {
-//   console.log('Printing all todos:');
-//   allTodos.forEach(function(todo) {
-//     console.log(todo);
-//   });
-//   quit();
-// });
